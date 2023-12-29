aef469119c935a6dd0fded0c26619241007fca9f
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-12-29 15:04:36 +0700

    Revert "add : cloud_firestore & wallpost"
    This reverts commit 19b9fa66d38f21475cac88162d4b796147c0d851.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tugasbesar/components/text_field.dart';
-import 'package:tugasbesar/components/wall_post.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,19 +20,6 @@ class _HomePageState extends State<HomePage>   {
   //sign user out
   void signOut(){
     FirebaseAuth.instance.signOut();
-  }
-
-  //post message
-  void postMessage(){
-    //only post if there is something in the textfield
-    if (textController.text.isNotEmpty){
-      //store in firebase
-      FirebaseFirestore.instance.collection('User Post').add({
-        'User Email': currentUser.email,
-        'Message': textController.text,
-        'TimeStamp': Timestamp.now(),
-      });
-    }
   }
 
   @override
@@ -64,60 +49,19 @@ class _HomePageState extends State<HomePage>   {
           child: Column(
             children: [
               //the wall
-              Expanded(
-                child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                  .collection("User Post")
-                  .orderBy(
-                    "TimeStamp", 
-                    descending: false,
-                  )
-                  .snapshots(),
-                  builder: (context, snapshot){
-                    if(snapshot.hasData){
-                      return ListView.builder(
-                        itemBuilder: (context, index){
-                        //get the message
-                          final post = snapshot.data!.docs[index];
-                          return WallPost(
-                            message: post['Message'], 
-                            user: post['User Email'], 
-                          );
-                        },
-                      );
-                    }else if(snapshot.hasError){
-                      return Center(
-                        child: Text('Error:${snapshot.error}'),
-                      );
-                    }
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                ),
-              ),
-
-
+          
               //post message
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  children: [
-                    //textfield
-                    Expanded(
-                      child: MyTextField(
-                        controller: textController,
-                        hintText: 'Write Something on The Wall',
-                        obscureText: false,
-                      ),
+              Row(
+                children: [
+                  //textfield
+                  Expanded(
+                    child: MyTextField(
+                      controller: textController,
+                      hintText: 'Write Something on The Wall',
+                      obscureText: false,
                     ),
-                
-                    //post button
-                    IconButton(
-                      onPressed: postMessage, 
-                      icon: const Icon(Icons.arrow_circle_up))
-                  ],
-                ),
+                  )
+                ],
               ),
 
               //logged in as
